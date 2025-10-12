d719259140bd71f2144e99c4ff9e577d79b8aee5
taohidkhan22 <taohid130@gmail.com>
2025-10-12 22:14:18 +0600

    SauceDemo Authentication & Add2cart using Codegen

---
+++ tests/Codegen Rcorder/codegen2recoder.spec.js
@@ -0,0 +1,29 @@
+
+//Expert module example
+//  exports.hello = function myFunction(){
+
+//     return " Exporting module example: ";
+
+//   }
+
+// const {hello}= require('filename')
+// console (hello())
+
+
+
+//sauce demo authentication & add to cart using code gen
+
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.locator('[data-test="logout-sidebar-link"]').click();
+});
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,14 @@
-// @ts-check
-import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+ const {hello} =require('./Codegen Rcorder/codegen2recoder.spec');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+ const {test,expect} = require('@playwright/test')
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+ test('My first test into playwright: ', async({page})=>{
+
+   await page.goto("https://www.google.com/")
+   await expect(page).toHaveTitle("Google")
+   
+
+ })
